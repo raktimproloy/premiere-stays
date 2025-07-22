af2f9b81e1b6ab98c8c3e6aba688779b055b84ad
raktimproloy <raktimproloy@gmail.com>
2025-07-22 12:58:07 +0600

    fix design issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,12 +22,23 @@ export default function Home() {
           href="/admin/dashboard"
           className="text-lg font-medium text-gray-700 hover:text-gray-900"
         >
-          Dashboard
+          Admin Dashboard
+        </Link>
+        <Link
+          href="/superadmin/dashboard"
+          className="text-lg font-medium text-gray-700 hover:text-gray-900"
+        >
+          Super Admin Dashboard
         </Link>
       </nav>
 
       <main className=" text-center font-bold">
-        <p>Email: test@example.com</p>
+        <h2>Admin Login</h2>
+        <p>Email: admin@example.com</p>
+        <p>Password: password123</p>
+        <br />
+        <h2>Super Admin Login</h2>
+        <p>Email: superadmin@example.com</p>
         <p>Password: password123</p>
       </main>
     </div>
